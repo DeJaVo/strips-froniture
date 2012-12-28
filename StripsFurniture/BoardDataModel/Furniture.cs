2aae38fd15dae23a08ea68fb2d48a7996eb866a7
dvirsegal84@gmail.com <dvirsegal84@gmail.com@940703a5-f3fc-b367-d51a-1ded9077c8b8>
2012-12-28 15:03:53 +0000

    Added furniture comapring support

--- StripsFurniture/BoardDataModel/Furniture.cs
+++ StripsFurniture/BoardDataModel/Furniture.cs
@@ -6,12 +6,20 @@ using System.Drawing;
 
 namespace BoardDataModel
 {
+
     public class Furniture
     {
-        private Rectangle furnitureDescription;
+        internal static int id=0;
+        private readonly Rectangle furnitureDescription;
         public Furniture(Rectangle rec)
         {
             furnitureDescription = rec;
+            id++;
+        }
+        
+        public int Id 
+        {
+            get { return id; }
         }
 
         public Rectangle Description
@@ -20,6 +28,64 @@ namespace BoardDataModel
             {
                 return furnitureDescription;
             }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((Furniture)obj);
+        }
+
+        protected bool Equals(Furniture other)
+        {
+            return furnitureDescription.Equals(other.furnitureDescription);
+        }
+
+        /// <summary>
+        /// Equals override
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        {
+            return furnitureDescription.GetHashCode();
+        }
+
+
+        /// <summary>
+        /// operator==
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool operator ==(Furniture a, Furniture b)
+        {
+            // If both are null, or both are same instance, return true.
+            if (ReferenceEquals(a, b))
+            {
+                return true;
+            }
+
+            // If one is null, but not both, return false.
+            if (((object)a == null) || ((object)b == null))
+            {
+                return false;
+            }
+
+            // Return true if the fields match:
+            return a.Description.X == b.Description .X && a.Description .Y == b.Description .Y && a.Id == b.Id;
+        }
+
+        /// <summary>
+        /// Operator !=
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        public static bool operator !=(Furniture a, Furniture b)
+        {
+            return !(a == b);
         }
     }
 }
