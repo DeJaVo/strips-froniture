158c589127639562da0ff83920a20e469978223f
dvirsegal84@gmail.com <dvirsegal84@gmail.com@940703a5-f3fc-b367-d51a-1ded9077c8b8>
2012-12-28 16:05:49 +0000

    added: 1. swap height and width (for rotate support) 2. Isempty which check for a given rectangle if slot is empty on room board 3. Rooms array 4. enum celltype which contains : empty,wall, door and allocated 5. Contructor which first build rooms array 6. Board is implemented as singelton

--- StripsFurniture/BoardDataModel/Board.cs
+++ StripsFurniture/BoardDataModel/Board.cs
@@ -9,9 +9,51 @@ namespace BoardDataModel
 {
     public class Board
     {
-        // stabs for GUI
-        public Board()
+        private static Board instance;
+        public enum CellType 
         {
+            Empty,
+            Allocated,
+            Door,
+            Wall
+        }
+
+        /// <summary>
+        /// Board
+        /// </summary>
+        public CellType[,] Rooms { get; set; }
+        
+        private Board()
+        {
+            //Vertical Seprated wall
+            for (int i = 0; i < 11; i++)
+            {
+                if (((i >= 2) && (i <= 3)) || ((i >= 7) && (i <= 10))) continue;
+                Rooms[i, 11] = CellType.Wall;
+            }
+
+            //Horizontal Separted wall
+            for (int j = 12; j < 20; j++)
+            {
+                Rooms[5, j] = CellType.Wall;
+            }
+
+            //Doors
+            for (int i = 0; i < 11; i++)
+            {
+                if (((i >= 2) && (i <= 3)) || ((i >= 7) && (i <= 10)))
+                {
+                    Rooms[i, 11] = CellType.Door;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Board singelton
+        /// </summary>
+        public static Board Instance
+        {
+            get { return instance ?? (instance = new Board()); }
         }
 
         /// <summary>
@@ -34,5 +76,54 @@ namespace BoardDataModel
             return false;
         }
         // stabs for GUI
+
+        /// <summary>
+        /// Swap width and height
+        /// </summary>
+        public void SwapWidthHeight(Furniture furniture)
+        {
+            Rectangle rectangle = furniture.Description;
+            var tempWidth = rectangle.Width;
+            rectangle.Width = rectangle.Height;
+            rectangle.Height = tempWidth;
+        }
+
+        /// <summary>
+        /// Checks if given rectangle is an empty slot on Rooms board
+        /// </summary>
+        /// <param name="rectangle"></param>
+        /// <returns></returns>
+        public bool IsEmpty(Rectangle rectangle)
+        {
+            int x = rectangle.X;
+            int y = rectangle.Y;
+            int width = rectangle.Width;
+            int height = rectangle.Height;
+            for (; x < x + height; x++)
+            {
+                for (; y < y + width; y++)
+                {
+                    if (Rooms[x, y] != CellType.Empty)
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        /// <summary>
+        /// Check if furniture is in board bounds
+        /// </summary>
+        /// <returns></returns>
+        public bool InBounds(Rectangle rectangle)
+        {
+            //Out of board bounds
+            if (rectangle.X <= 0 || rectangle.Y <= 0 || rectangle.X > 11 || rectangle.Y > 20)
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
