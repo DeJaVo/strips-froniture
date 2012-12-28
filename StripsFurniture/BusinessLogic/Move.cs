941989a8076e8a5a242371300d3c430aa8b7c7ab
dvirsegal84@gmail.com <dvirsegal84@gmail.com@940703a5-f3fc-b367-d51a-1ded9077c8b8>
2012-12-28 15:37:57 +0000

    Move - added direction enum and property, in how many steps in direction Rotate - added Rotation direction enum and property and angle property

--- StripsFurniture/BusinessLogic/Move.cs
+++ StripsFurniture/BusinessLogic/Move.cs
@@ -5,7 +5,27 @@ using System.Text;
 
 namespace BusinessLogic
 {
-    public class Move
+    /// <summary>
+    /// Moving Direction
+    /// </summary>
+    public enum Direction
     {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    public class Move:Operation
+    {
+        /// <summary>
+        /// returns direction
+        /// </summary>
+        public Direction Direction { get; set; }
+
+        /// <summary>
+        /// returns how many steps to move in direction
+        /// </summary>
+        public int HowManyStepsInDirection { get; set; }
     }
 }
