77a881af30a0a065a6bada07fb1fa1e6a0e5bbbe
dvirsegal84@gmail.com <dvirsegal84@gmail.com@940703a5-f3fc-b367-d51a-1ded9077c8b8>
2012-12-28 21:36:21 +0000

    Added functionaity for move and rotate: 1. Checking if rotating is valid 2. checking if move is valid

--- StripsFurniture/BusinessLogic/Move.cs
+++ StripsFurniture/BusinessLogic/Move.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using BoardDataModel;
 
 namespace BusinessLogic
 {
@@ -18,6 +20,7 @@ namespace BusinessLogic
 
     public class Move:Operation
     {
+        private readonly Board board = Board.Instance;
         /// <summary>
         /// returns direction
         /// </summary>
@@ -27,5 +30,51 @@ namespace BusinessLogic
         /// returns how many steps to move in direction
         /// </summary>
         public int HowManyStepsInDirection { get; set; }
+
+        public bool IsValidMove(Furniture furniture)
+        {
+            //calculate new rectangle
+            var newdestRectangle = new Rectangle
+                {
+                    Width = furniture.Description.Width,
+                    Height = furniture.Description.Height
+                };
+            switch (Direction)
+            {
+                    case Direction.Down:
+                    {
+                        newdestRectangle.X = furniture.Description.X + HowManyStepsInDirection;
+                        break;
+                    }
+                case Direction.Up:
+                    {
+                        newdestRectangle.X = furniture.Description.X - HowManyStepsInDirection;
+                        break;
+                    }
+                case Direction.Left:
+                    {
+                        newdestRectangle.Y = furniture.Description.Y - HowManyStepsInDirection;
+                        break;
+                    }
+                case Direction.Right:
+                    {
+                        newdestRectangle.Y = furniture.Description.Y + HowManyStepsInDirection;
+                        break;
+                    }
+
+            }
+
+            //inbounds
+            if (board.InBounds(newdestRectangle))
+            {
+                return false;
+            }
+            //isempty
+            if (board.IsEmpty(newdestRectangle))
+            {
+                return false;
+            }
+            return true;
+        }
     }
 }
--- StripsFurniture/BusinessLogic/Rotate.cs
+++ StripsFurniture/BusinessLogic/Rotate.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using BoardDataModel;
 
 namespace BusinessLogic
 {
@@ -10,8 +12,15 @@ namespace BusinessLogic
         ClockWise,
         CounterClockWise
     }
+
+    public enum Orientation
+    {
+        Horizontal,
+        Vertical
+    }
     public class Rotate :Operation
     {
+        private readonly Board board = Board.Instance;
         /// <summary>
         /// returns direction
         /// </summary>
@@ -21,5 +30,87 @@ namespace BusinessLogic
         /// Angle
         /// </summary>
         public int Angle { get; set; }
+
+        /// <summary>
+        /// Check if Rotation is valid
+        /// </summary>
+        /// <param name="furniture"></param>
+        /// <param name="direction"></param>
+        /// <returns></returns>
+        public bool IsValidRotate(Furniture furniture,RotationDirection direction)
+        {
+            int width = furniture.Description.Width;
+            int height = furniture.Description.Height;
+            int x = furniture.Description.X;
+            int y = furniture.Description.Y;
+            Rectangle temp1, temp2;
+            Orientation orientation = width > height ? Orientation.Horizontal : Orientation.Vertical;
+            CheckRotateByDirection(furniture, direction, orientation, out temp1, out temp2);
+            if (!(board.InBounds(temp1) || board.InBounds(temp2)))
+            {
+                return false;
+            }
+            if(!(board.IsEmpty(temp1)||board.IsEmpty(temp2)))
+            {
+                return false;
+            }
+            return true;
+        }
+
+        /// <summary>
+        ///  Check rotation rectangles for bounds and empty.
+        /// </summary>
+        /// <param name="furniture"></param>
+        /// <param name="direction"></param>
+        /// <param name="orientation"></param>
+        /// <param name="temp1"></param>
+        /// <param name="temp2"></param>
+        /// <returns></returns>
+        public void CheckRotateByDirection(Furniture furniture,RotationDirection direction,Orientation orientation,out Rectangle temp1, out Rectangle temp2)
+        {
+            int width = furniture.Description.Width;
+            int height = furniture.Description.Height;
+            int x = furniture.Description.X;
+            int y = furniture.Description.Y;
+            int value = orientation == Orientation.Horizontal ? width : height;
+            switch (direction)
+            {
+                    case RotationDirection.ClockWise:
+                    {
+                        if (orientation == Orientation.Horizontal)
+                        {
+                            temp1 = new Rectangle(x - (value) / 2, y + (value) / 2, value / 2,
+                                                      (int)Math.Ceiling((double)value / 2));
+                            temp2 = new Rectangle(x + 1, y +(int)Math.Ceiling((double)value / 2), value / 2, (int)Math.Ceiling((double)value / 2));
+                        }
+                        else if (orientation == Orientation.Vertical)
+                        {
+                            temp1 = new Rectangle(x+1, y - (value) / 2, (int)Math.Ceiling((double)value / 2), (value / 2));
+                            temp2 = new Rectangle(x + (int)Math.Ceiling((double)value / 2), y + width,
+                                                      (int)Math.Ceiling((double)value / 2), (value) / 2);
+                        }
+                        break;
+                    }
+                    case RotationDirection.CounterClockWise:
+                    {
+                        if (orientation == Orientation.Horizontal)
+                        {
+                            temp1 = new Rectangle(x - (value)/2, y + (value)/2, value/2,
+                                                      (int) Math.Ceiling((double) value/2));
+                            temp2 = new Rectangle(x + 1, y, value/2, (int) Math.Ceiling((double) value/2));
+                        }
+                        else if(orientation==Orientation.Vertical)
+                        {
+                            temp1 = new Rectangle(x, y - (value)/2, (int) Math.Ceiling((double) value/2), (value/2));
+                            temp2 = new Rectangle(x + (int) Math.Ceiling((double) value/2), y + width,
+                                                      (int) Math.Ceiling((double) value/2), (value)/2);
+                        }
+                        break;
+                    }
+            }
+
+            temp1 = new Rectangle();
+            temp2 = new Rectangle();
+        }
     }
 }
