e568bbcd966731f5de36a567f128463690d4fb75
sheirabh@gmail.com <sheirabh@gmail.com@940703a5-f3fc-b367-d51a-1ded9077c8b8>
2012-12-29 14:36:22 +0000

    added: 1. function that splits list of predicates into 2 list of predicates according to kind. 2 function that orders a list of predicates modified: 1. moved Predicate into Heuristics.

--- StripsFurniture/BusinessLogic/Strips.cs
+++ StripsFurniture/BusinessLogic/Strips.cs
@@ -1,63 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-
+﻿using System.Collections.Generic;
 using BoardDataModel;
+using Heuristics;
+
+
 namespace BusinessLogic
 {
     public class Strips
     {
+       Heuristic heuristic = new Heuristic();
+        
         //maybe hold a list with all final operations- usefull for get next operation. 
         public Strips(Board board)
         {
             IList<Predicate> startState = new List<Predicate>();
             IList<Predicate> goalState = new List<Predicate>();
-            startState = FindStartStatePredicates(board);
-            goalState = FindGoalStatePredicates(board);
+            startState = heuristic.FindStartStatePredicates(board);
+            goalState = heuristic.FindGoalStatePredicates(board);
             //calculate predicates of start positions and to state
             //calculate predicates of final board and add to the work stack
 
         }
 
-        /// <summary>
-        /// finds all predicates that describe current position.
-        /// </summary>
-        /// <param name="board"></param>
-        /// <returns>returns a list of predicates </returns>
-        private IList<Predicate> FindStartStatePredicates(Board board)
-        {
-            List<Predicate> predicates = new List<Predicate>();
-            foreach (KeyValuePair<Furniture, Rectangle> pair in board.furnitureDestination)
-            {                
-                int id = pair.Key.ID;
-                Rectangle rect = pair.Key.Description;               
-                Predicate newPredicate = new PLocation(id,rect);
-                predicates.Add(newPredicate);
-            }
-            return predicates;
-        }
-
-        /// <summary>
-        /// finds all predicates that describe the goal positions
-        /// </summary>
-        /// <param name="board"></param>
-        /// <returns>return a list of predicates</returns>
-        private IList<Predicate> FindGoalStatePredicates(Board board)
-        {
-            IList<Predicate> predicates = new List<Predicate>();
-            foreach ( KeyValuePair<Furniture, Rectangle> pair in board.furnitureDestination )
-            {
-                IList<Rectangle> rectangels=new List<Rectangle>();
-                int id = pair.Key.ID;
-                Rectangle rect = pair.Value;
-                rectangels.Add(rect);
-                Predicate newPredicate = new PClean(rectangels);
-                predicates.Add(newPredicate);
-            }
-            return predicates;
-        }
+       
 
         /// <summary>
         /// returns the next move to execute
